0d91ff671c8634ac9eb5537dfe1ecf36154e2bcf
erikrios <erikriosetiawan49@gmail.com>
2020-02-22 19:51:17 +0700

    Classyfying the class into packages

--- app/src/main/java/com/erikriosetiawan/shinobilist/views/DetailsActivity.kt
+++ app/src/main/java/com/erikriosetiawan/shinobilist/views/DetailsActivity.kt
@@ -1,12 +1,13 @@
-package com.erikriosetiawan.shinobilist
+package com.erikriosetiawan.shinobilist.views
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.erikriosetiawan.shinobilist.R
 
-class MainActivity : AppCompatActivity() {
+class DetailsActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_details)
     }
 }
--- app/src/main/java/com/erikriosetiawan/shinobilist/views/MainActivity.kt
+++ app/src/main/java/com/erikriosetiawan/shinobilist/views/MainActivity.kt
@@ -1,12 +1,13 @@
-package com.erikriosetiawan.shinobilist
+package com.erikriosetiawan.shinobilist.views
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.erikriosetiawan.shinobilist.R
 
-class DetailsActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_details)
+        setContentView(R.layout.activity_main)
     }
 }
