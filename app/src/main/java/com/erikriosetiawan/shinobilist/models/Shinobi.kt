7a19ad85c6202a73128e206dd87cd61fd4d18fac
erikrios <erikriosetiawan49@gmail.com>
2020-02-22 19:53:20 +0700

    Add data class shinobi

---
+++ app/src/main/java/com/erikriosetiawan/shinobilist/models/Shinobi.kt
@@ -0,0 +1,8 @@
+package com.erikriosetiawan.shinobilist.models
+
+data class Shinobi(
+    var id: Int = -1,
+    var name: String? = null,
+    var village: String? = null,
+    var description: String? = null
+)
