3ba7e0fda8014ad4ee7cff2e0f960949b95dd283
erikrios <erikriosetiawan49@gmail.com>
2020-02-22 20:12:37 +0700

    Add shinbi adapter for the recyclerview

---
+++ app/src/main/java/com/erikriosetiawan/shinobilist/adapters/ShinobiAdapter.kt
@@ -0,0 +1,35 @@
+package com.erikriosetiawan.shinobilist.adapters
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.erikriosetiawan.shinobilist.databinding.ListShinobiBinding
+import com.erikriosetiawan.shinobilist.models.Shinobi
+
+class ShinobiAdapter(var context: Context, var shinobis: MutableList<Shinobi>) :
+    RecyclerView.Adapter<ShinobiAdapter.ViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val layoutInflater = LayoutInflater.from(context)
+        val binding = ListShinobiBinding.inflate(layoutInflater, parent, false)
+        return ViewHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.bind(shinobis[position]) {
+            // Intent code here
+        }
+    }
+
+    override fun getItemCount(): Int = shinobis.size
+
+    inner class ViewHolder(var binding: ListShinobiBinding) :
+        RecyclerView.ViewHolder(binding.root) {
+        fun bind(shinobi: Shinobi, clickListener: (Shinobi) -> Unit) {
+            binding.textViewShinobiName.text = shinobi.name
+            itemView.setOnClickListener { clickListener(shinobi) }
+        }
+    }
+}
