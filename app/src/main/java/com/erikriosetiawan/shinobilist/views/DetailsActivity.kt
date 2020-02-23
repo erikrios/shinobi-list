1a67f19648e1c3b9a5c01f435fd58adfa2bf7133
erikrios <erikriosetiawan49@gmail.com>
2020-02-23 19:01:40 +0700

    Initialize action bar and options menu

--- app/src/main/java/com/erikriosetiawan/shinobilist/views/DetailsActivity.kt
+++ app/src/main/java/com/erikriosetiawan/shinobilist/views/DetailsActivity.kt
@@ -1,6 +1,8 @@
 package com.erikriosetiawan.shinobilist.views
 
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import com.erikriosetiawan.shinobilist.R
@@ -13,5 +15,25 @@ class DetailsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_details)
+        setActionBar()
+    }
+
+    // Set support action bar
+    private fun setActionBar() {
+        setSupportActionBar(binding.toolbar)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_details, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.action_menu_delete -> {
+                // Do something
+            }
+        }
+        return super.onOptionsItemSelected(item)
     }
 }
