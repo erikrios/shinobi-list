54ac78b7c485313670dd4790e94f970b750e8b7f
erikrios <erikriosetiawan49@gmail.com>
2020-02-23 19:18:31 +0700

    Add a function to add a new shonobi

--- app/src/main/java/com/erikriosetiawan/shinobilist/views/DetailsActivity.kt
+++ app/src/main/java/com/erikriosetiawan/shinobilist/views/DetailsActivity.kt
@@ -6,7 +6,9 @@ import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import com.erikriosetiawan.shinobilist.R
+import com.erikriosetiawan.shinobilist.data.SampleData
 import com.erikriosetiawan.shinobilist.databinding.ActivityDetailsBinding
+import com.erikriosetiawan.shinobilist.models.Shinobi
 
 class DetailsActivity : AppCompatActivity() {
 
@@ -16,6 +18,10 @@ class DetailsActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_details)
         setActionBar()
+        binding.buttonSave.setOnClickListener {
+            addNewData()
+            finish()
+        }
     }
 
     // Set support action bar
@@ -35,5 +41,35 @@ class DetailsActivity : AppCompatActivity() {
             }
         }
         return super.onOptionsItemSelected(item)
+    }
+
+    private fun addNewData() {
+        var isEmptyField = false
+
+        if (binding.editTextShinobiName.text.toString().isEmpty()) {
+            isEmptyField = true
+            binding.editTextShinobiName.error = getString(R.string.shinobi_name_error_message)
+        }
+
+        if (binding.editTextShinobiVillageFrom.text.toString().isEmpty()) {
+            isEmptyField = true
+            binding.editTextShinobiVillageFrom.error =
+                getString(R.string.shinobi_village_error_message)
+        }
+
+        if (binding.editTextShinobiDescription.text.toString().isEmpty()) {
+            isEmptyField = true
+            binding.editTextShinobiDescription.error =
+                getString(R.string.shinobi_description_error_message)
+        }
+
+        if (!isEmptyField) {
+            val shinobi = Shinobi()
+            shinobi.name = binding.editTextShinobiName.text.toString()
+            shinobi.id = SampleData.getShinobis.size
+            shinobi.village = binding.editTextShinobiVillageFrom.text.toString()
+            shinobi.description = binding.editTextShinobiDescription.text.toString()
+            SampleData.addShinobi(shinobi)
+        }
     }
 }
