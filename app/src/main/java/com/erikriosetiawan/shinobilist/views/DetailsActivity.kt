d78cf528cdad54d0f6f1933fa102ac59f4bfdc8d
erikrios <erikriosetiawan49@gmail.com>
2020-02-23 18:57:29 +0700

    Integrate data binding with the details activity

--- app/src/main/java/com/erikriosetiawan/shinobilist/views/DetailsActivity.kt
+++ app/src/main/java/com/erikriosetiawan/shinobilist/views/DetailsActivity.kt
@@ -1,13 +1,17 @@
 package com.erikriosetiawan.shinobilist.views
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
 import com.erikriosetiawan.shinobilist.R
+import com.erikriosetiawan.shinobilist.databinding.ActivityDetailsBinding
 
 class DetailsActivity : AppCompatActivity() {
 
+    private lateinit var binding: ActivityDetailsBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_details)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_details)
     }
 }
