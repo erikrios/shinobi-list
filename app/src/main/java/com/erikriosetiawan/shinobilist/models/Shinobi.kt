143192a268150665efb225d8ae1ccba2ff519290
erikrios <erikriosetiawan49@gmail.com>
2020-02-24 11:41:37 +0700

    Add a parcelable to model class

--- app/src/main/java/com/erikriosetiawan/shinobilist/models/Shinobi.kt
+++ app/src/main/java/com/erikriosetiawan/shinobilist/models/Shinobi.kt
@@ -1,8 +1,12 @@
 package com.erikriosetiawan.shinobilist.models
 
+import android.os.Parcelable
+import kotlinx.android.parcel.Parcelize
+
+@Parcelize
 data class Shinobi(
     var id: Int = -1,
     var name: String? = null,
     var village: String? = null,
     var description: String? = null
-)
+) : Parcelable
