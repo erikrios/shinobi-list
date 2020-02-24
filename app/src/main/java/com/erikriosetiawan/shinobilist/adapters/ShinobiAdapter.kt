eefbf49dbcd67fece98a1e730baef64c1ad2f5cc
erikrios <erikriosetiawan49@gmail.com>
2020-02-24 11:57:59 +0700

    Optimize the code

--- app/src/main/java/com/erikriosetiawan/shinobilist/adapters/ShinobiAdapter.kt
+++ app/src/main/java/com/erikriosetiawan/shinobilist/adapters/ShinobiAdapter.kt
@@ -4,7 +4,6 @@ import android.content.Context
 import android.content.Intent
 import android.view.LayoutInflater
 import android.view.ViewGroup
-import android.widget.Toast
 import androidx.recyclerview.widget.RecyclerView
 import com.erikriosetiawan.shinobilist.databinding.ListShinobiBinding
 import com.erikriosetiawan.shinobilist.models.Shinobi
