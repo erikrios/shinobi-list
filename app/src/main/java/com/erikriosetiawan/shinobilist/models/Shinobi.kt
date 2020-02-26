48a1787130c9aa16bdf5083c9e9b573bb6dd08cb
erikrios <erikriosetiawan49@gmail.com>
2020-02-26 17:21:17 +0700

    Create shinobi entity

--- app/src/main/java/com/erikriosetiawan/shinobilist/models/Shinobi.kt
+++ app/src/main/java/com/erikriosetiawan/shinobilist/models/Shinobi.kt
@@ -1,12 +1,23 @@
 package com.erikriosetiawan.shinobilist.models
 
 import android.os.Parcelable
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
 import kotlinx.android.parcel.Parcelize
 
 @Parcelize
+@Entity(tableName = "shinobi_table")
 data class Shinobi(
+    @PrimaryKey(autoGenerate = true)
     var id: Int = -1,
+
+    @ColumnInfo(name = "name")
     var name: String? = null,
+
+    @ColumnInfo(name = "village")
     var village: String? = null,
+
+    @ColumnInfo(name = "description")
     var description: String? = null
 ) : Parcelable
