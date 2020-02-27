79c6e882340d9a52c658ecb07a40ba5e0b1c83a1
erikrios <erikriosetiawan49@gmail.com>
2020-02-27 16:04:47 +0700

    Get all shinobi from the database

--- app/src/main/java/com/erikriosetiawan/shinobilist/views/MainActivity.kt
+++ app/src/main/java/com/erikriosetiawan/shinobilist/views/MainActivity.kt
@@ -9,18 +9,28 @@ import androidx.recyclerview.widget.LinearLayoutManager
 import com.erikriosetiawan.shinobilist.R
 import com.erikriosetiawan.shinobilist.adapters.ShinobiAdapter
 import com.erikriosetiawan.shinobilist.data.SampleData
+import com.erikriosetiawan.shinobilist.database.ShinobiDatabase
 import com.erikriosetiawan.shinobilist.databinding.ActivityMainBinding
+import com.erikriosetiawan.shinobilist.models.Shinobi
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
-    private lateinit var adapter: ShinobiAdapter
+    private var adapter: ShinobiAdapter? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
 
-        adapter = ShinobiAdapter(this, SampleData.getShinobis)
+        GlobalScope.launch {
+            val database = ShinobiDatabase.getInstace(this@MainActivity).shinobiDatabaseDao
+            val myData = database.getAllShinobi()
+            runOnUiThread {
+                adapter = ShinobiAdapter(this@MainActivity, myData as MutableList<Shinobi>)
+            }
+        }
         binding.recyclerViewShinobi.layoutManager =
             LinearLayoutManager(this@MainActivity, LinearLayoutManager.VERTICAL, false)
         binding.recyclerViewShinobi.adapter = adapter
@@ -28,6 +38,7 @@ class MainActivity : AppCompatActivity() {
         binding.floatingActionButtonAdd.setOnClickListener {
             addShinobi()
         }
+
     }
 
     private fun addShinobi() {
@@ -37,7 +48,7 @@ class MainActivity : AppCompatActivity() {
 
     override fun onResume() {
         super.onResume()
-        adapter.notifyDataSetChanged()
+        adapter?.notifyDataSetChanged()
         for (i in 0 until SampleData.getShinobis.size) {
             val shinobi = SampleData.getShinobis[i]
             Log.d(
